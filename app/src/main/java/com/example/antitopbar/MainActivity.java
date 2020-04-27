383733ea1cfea854212ad47a5a0d9ddb85296fae
Bruno Volpini <fabrivol@hotmail.com>
2020-04-27 11:09:17 -0300

    First Commit

---
+++ app/src/main/java/com/example/antitopbar/MainActivity.java
@@ -0,0 +1,112 @@
+package com.example.antitopbar;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    /* SOURCE CODE https://stackoverflow.com/questions/25284233/prevent-status-bar-for-appearing-android-modified */
+
+    public static final int OVERLAY_PERMISSION_REQ_CODE = 4545;
+    protected CustomViewGroup blockingView = null;
+
+    @Override
+    protected void onDestroy() {
+
+        super.onDestroy();
+
+        if (blockingView!=null) {
+            WindowManager manager = ((WindowManager) getApplicationContext().getSystemService(Context.WINDOW_SERVICE));
+            manager.removeView(blockingView);
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+            if (!Settings.canDrawOverlays(this)) {
+                Toast.makeText(this, "Please give my app this permission!", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName()));
+                startActivityForResult(intent, OVERLAY_PERMISSION_REQ_CODE);
+            } else {
+                disableStatusBar();
+            }
+        } else {
+            disableStatusBar();
+        }
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == OVERLAY_PERMISSION_REQ_CODE) {
+            if (!Settings.canDrawOverlays(this)) {
+                Toast.makeText(this, "User can access system settings without this permission!", Toast.LENGTH_SHORT).show();
+            }
+            else
+            { disableStatusBar();
+            }
+        }
+    }
+
+    protected void disableStatusBar() {
+
+        WindowManager manager = ((WindowManager) getApplicationContext().getSystemService(Context.WINDOW_SERVICE));
+
+        WindowManager.LayoutParams localLayoutParams = new WindowManager.LayoutParams();
+        localLayoutParams.type = WindowManager.LayoutParams.TYPE_SYSTEM_ERROR;
+        localLayoutParams.gravity = Gravity.TOP;
+        localLayoutParams.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE |
+
+                // this is to enable the notification to receive touch events
+                WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL |
+
+                // Draws over status bar
+                WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN;
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int height = displayMetrics.heightPixels;
+        int width = displayMetrics.widthPixels;
+
+        Log.i("MATCH_PARENT", WindowManager.LayoutParams.MATCH_PARENT + ""); //
+        Log.i("height", height + ""); // 600
+        Log.i("width", width + ""); // 1024
+        Log.i("scaledDensity",  getResources().getDisplayMetrics().scaledDensity + ""); // 60.0
+        Log.i("scaledDensity 2", 40 * getResources().getDisplayMetrics().scaledDensity + ""); // 60.0
+/*
+        localLayoutParams.width = WindowManager.LayoutParams.MATCH_PARENT;
+        localLayoutParams.height = (int) (40 * getResources().getDisplayMetrics().scaledDensity);*/
+
+        localLayoutParams.width = 905;
+        localLayoutParams.height = (int) 60.0;
+
+        localLayoutParams.format = PixelFormat.TRANSPARENT;
+
+        blockingView = new CustomViewGroup(this);
+        manager.addView(blockingView, localLayoutParams);
+    }
+
+
+
+}
